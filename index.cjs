6104d8c43240d1ecd67104b6e7a8a70517e7af10
Johnny Parra <juanparra1996@gmail.com>
2024-05-17 22:41:40 -0700

    Mysql connection fixed

--- index.cjs
+++ index.cjs
@@ -22,14 +22,12 @@ const corsOptions = {
    optionSuccessStatus: 200,
 };
 
-console.log('test before server');
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
-console.log('test after server');
 
 app.use(cors(corsOptions));
 
